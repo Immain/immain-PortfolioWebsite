294b36f0aa223d24342228123dc429c79a902e96
Ian Main <ian@pandadatasystems.com>
2022-05-29 16:09:10 -0700

    Add files via upload

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -4,8 +4,7 @@ export const projects = [
     description: "I was tasked with building South Texas Pain and Spines new website, after a previous web development company destroyed their site. South Texas Pain and Spine is based out of Corpus Christi, Texas. This site was built using Wordpress and PHP",
     image: '/images/1.png',
     tags: ['CMS', 'Wordpress', 'PHP', 'MongoDB', 'Express'],
-    source: 'https://google.com',
-    visit: 'https://southtexaspas.com/',
+    source: 'https://southtexaspas.com/',
     id: 0,
   },
   {
@@ -13,8 +12,7 @@ export const projects = [
     description:"While building it you're going to learn many advanced React & JavaScript topics, as well as how to use Stripe for card transactions. On top of that, at the end of the video, you will have this unique and complex webshop app that you will be able to add to your portfolio. And trust me, e-commerce applications are impressive.",
     image: '/images/2.png',
     tags: ['CMS','Wordpress', 'PHP', 'BootStrap'],
-    source: 'https://google.com',
-    visit: 'https://aavems.com/',
+    source: 'https://aavems.com/',
     id: 1,
   },
   {
@@ -22,8 +20,7 @@ export const projects = [
     description: "A spotify 2.0 hobby build I did for fun to challenge myself and test my skills. This project is a work in progress and is not the complete version, some functions may not work.",
     image: '/images/3.png',
     tags: ['Next.js', 'NextAuth.js', 'Middleware', 'Recoil'],
-    source: 'https://github.com/Immain/PDS-Spotify-May2022',
-    visit: 'https://google.com',
+    visit: 'https://github.com/Immain/PDS-Spotify-May2022',
     id: 2,
   },
   {
@@ -31,8 +28,8 @@ export const projects = [
     description: "This is a work in progress, I'm working on a new app that I'm building and it's a very special proejct to me. I'm hoping to have a fully functional and working app in the next few months.",
     image: '/images/5.jpg',
     tags: ['React', 'Recoil', 'Firebase'],
-    source: 'https://github.com/Immain/Midnight-App',
-    visit: 'https://midnighttechnologies.com/',
+    source: 'https://midnighttechnologies.com/',
+    visit: 'https://github.com/Immain/Midnight-App',
     id: 3,
   },
 ];
@@ -40,7 +37,7 @@ export const projects = [
 export const TimeLineData = [
   { year: 2016, text: 'Boeing - Help Desk Analyst', },
   { year: 2017, text: 'American Ambulance Visalia - Information Technology Specialist', },
-  { year: 2017, text: 'Robert Half Technology - IT Administrator', },
   { year: 2019, text: 'Midnight Technologies - Information Technology Specialist', },
   { year: 2022, text: 'Started as a freelance developer', },
+  { year: 2023, text: 'Founded High Sierra Software', },
 ];
